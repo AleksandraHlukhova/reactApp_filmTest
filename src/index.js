b4ef3fe1366bcfe63ce693bb4e3deead82881adc
AleksandraHlukhova <a0993715642@gmail.com>
2020-04-13 12:59:15 +0300

    create repository

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "bootstrap/dis/css/bootstrap.min.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const movie = {
+  title: "Звездные войны",
+  vote: 9,
+  img: "https://vesti.ua/wp-content/uploads/2019/12/Skrestilis-800x530.jpg",
+  overview: 'loremzdrhzhxjjxfxj'
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// function MovieItem(props) {
+//   console.log(props);
+//   const {data: {title, vote, img}} = props;
+//   return(
+//     <div>
+//     <p>{title}</p>
+//     <p>{vote}</p>
+//     <img src={img} alt="title"/>
+//     </div>
+//   );
+// }
+
+class MovieItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+      like: false
+    }
+  }
+  toggleOverView = () => {
+    this.setState({
+      show: !this.state.show,
+    })
+  }
+  handleLike = () => {
+    this.setState({
+      like: !this.state.like,
+    })
+  }
+  render() {
+    const {data: {title, vote, img, overview}} = this.props;
+    console.log('state', this.state);
+    console.log(this);
+
+    return(
+      <div>
+        <div style={{margin: "0 auto", width: "500px"}}>
+          <div>
+            <img src={img} alt="title" style={{maxWidth: "100%"}}/>
+          </div>
+          <p>{title}</p>
+          <p>{vote}</p>
+          <div style={{display: "flex", justifyContent: "space-between"}}>
+            <button type="button" onClick={this.toggleOverView}>{this.state.show ? 'hide' : 'show'}</button>
+            <button type="button" onClick={this.handleLike} className={this.state.like ? 'btn_like' : ''}
+            >{this.state.like ? 'unlike' : 'like'}</button>
+          </div>
+          {this.state.show ? <p>{overview}</p> : null}
+        </div>
+    </div>
+    );
+  }
+}
+
+function App() { 
+  return (
+    <MovieItem data={movie}/>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
